656716e5129725a4992f1a010ababf52162a96f4
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-20 20:44:26 -0300

    Comienzo de desarrollo del TP4

---
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/Tp4BazanFabricioAgustinApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4BazanFabricioAgustinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4BazanFabricioAgustinApplication.class, args);
+	}
+
+}
