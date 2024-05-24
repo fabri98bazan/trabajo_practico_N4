6b96b77ffbe64672ae7cbb0c0b4b0725eba59eaa
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-24 19:28:21 -0300

    Agregado de atributos a las clases

--- tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,5 +1,66 @@
 package ar.edu.unju.fi.model;
 
-public class Materia {
 
+public class Materia {
+	private int cod;
+	private String nombre_materia;
+	private int cant_horas;
+	private boolean modalidad;
+	private Docente docente;
+	private Carrera carrera;
+	
+	public Materia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getNombre_materia() {
+		return nombre_materia;
+	}
+
+	public void setNombre_materia(String nombre_materia) {
+		this.nombre_materia = nombre_materia;
+	}
+
+	public int getCant_horas() {
+		return cant_horas;
+	}
+
+	public void setCant_horas(int cant_horas) {
+		this.cant_horas = cant_horas;
+	}
+
+	public boolean isModalidad() {
+		return modalidad;
+	}
+
+	public void setModalidad(boolean modalidad) {
+		this.modalidad = modalidad;
+	}
+
+	public Docente getDocente() {
+		return docente;
+	}
+
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+
+	public Carrera getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+	
+	
+	
 }
