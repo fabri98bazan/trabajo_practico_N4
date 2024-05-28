f89412058e98bbe6247acd03519580cbe99adad5
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-28 10:27:20 -0300

    Agregado de las fragmentaciones

--- tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
 	@GetMapping("/index")
-	public String getIndexPage() {
+	public String getIndexPage(Model model) {
+		model.addAttribute("titulo","Inicio");
 		return "index";
 	}
 }
