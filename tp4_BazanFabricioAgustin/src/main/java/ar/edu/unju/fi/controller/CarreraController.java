5f7844ee24df67c73d151072341241b05eaf325a
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-29 21:00:21 -0300

    Finalizado el trabajo de Carreras

--- tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -1,16 +1,24 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.collections.CollectionCarrera;
+import ar.edu.unju.fi.model.Carrera;
 
 @Controller
 @RequestMapping("/carrera")
 public class CarreraController {
-
+	@Autowired
+	private Carrera carrera;
+	
 	@GetMapping("/listado")
 	public String getCarrerasPage(Model model) {
 		model.addAttribute("carreras", CollectionCarrera.getCarreras());
@@ -18,4 +26,44 @@ public class CarreraController {
 		return "carreras";
 	}
 	
+	@GetMapping("/nuevo")
+	public String getNuevaCarreraPage(Model model) {
+		boolean edicion=false;
+		model.addAttribute("carrera", carrera);
+		model.addAttribute("edicion", edicion);
+		model.addAttribute("titulo", "Nueva carrera");
+		return "carrera";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView guardarCarrera(@ModelAttribute("carrera") Carrera carrera) {
+		ModelAndView modelView=new ModelAndView("carreras");
+		carrera.setEstado(true);
+		CollectionCarrera.agregarCarrera(carrera);
+		modelView.addObject("carreras",CollectionCarrera.getCarreras());
+		return modelView;
+	}
+	
+	@GetMapping("/modificar/{cod_carrera}")
+	public String getModificarCarreraPage(Model model, @PathVariable(value="cod_carrera") int codigo) {
+		Carrera carreraEncontrada= new Carrera();
+		boolean edicion=true;
+		carreraEncontrada=CollectionCarrera.buscarCarrera(codigo);
+		model.addAttribute("edicion", edicion);
+		model.addAttribute("carrera", carreraEncontrada);
+		model.addAttribute("titulo","Modificar Carrera");
+		return "carrera";
+	}
+	
+	@PostMapping("/modificar")
+	public String modificarCarrera(@ModelAttribute("carrera") Carrera carrera) {
+		CollectionCarrera.modificarCarrera(carrera);
+		return "redirect:/carrera/listado";
+	}
+	
+	@GetMapping("/eliminar/{cod_carrera}")
+	public String eliminarCarrera(@PathVariable(value="cod_carrera") int codigo) {
+		CollectionCarrera.eliminarCarrera(codigo);
+		return "redirect:/carrera/listado";
+	}
 }
