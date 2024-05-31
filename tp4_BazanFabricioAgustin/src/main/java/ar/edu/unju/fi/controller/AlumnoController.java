f879d373f55179122668eb0209688fc9adcdba62
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-31 06:22:26 -0300

    Iniciando trabajo de AlumnoController

---
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.collections.CollectionAlumno;
+import ar.edu.unju.fi.model.Alumno;
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnoController {
+
+}
