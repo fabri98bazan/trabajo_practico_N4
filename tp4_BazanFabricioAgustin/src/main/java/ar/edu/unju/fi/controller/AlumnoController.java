7496681c5d77c0b93c642cdbd7eca9983dc5c9ca
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-05-31 06:34:18 -0300

    Terminado el AlumnoController

--- tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -16,5 +16,54 @@ import ar.edu.unju.fi.model.Alumno;
 @Controller
 @RequestMapping("/alumno")
 public class AlumnoController {
-
+	@Autowired
+	private Alumno alumno;
+	
+	@GetMapping("/listado")
+	public String getAlumnosPage(Model model) {
+		model.addAttribute("alumnos", CollectionAlumno.getAlumnos());
+		model.addAttribute("titulo","Alumnos");
+		return "alumnos";
+	}
+	
+	@GetMapping("/nuevo")
+	public String getNuevoAlumnoPage(Model model) {
+		boolean edicion=false;
+		model.addAttribute("docente", alumno);
+		model.addAttribute("edicion", edicion);
+		model.addAttribute("titulo", "Nuevo alumno");
+		return "alumno";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView guardarAlumno(@ModelAttribute("alumno") Alumno alumno) {
+		ModelAndView modelView=new ModelAndView("alumnos");
+		CollectionAlumno.agregarAlumno(alumno);
+		modelView.addObject("alumnos",CollectionAlumno.getAlumnos());
+		return modelView;
+	}
+	
+	@GetMapping("/modificar/{LU}")
+	public String getModificarAlumnoPage(Model model, @PathVariable(value="LU") int lu) {
+		Alumno alumnoEncontrado= new Alumno();
+		boolean edicion=true;
+		alumnoEncontrado=CollectionAlumno.buscarAlumnos(lu);
+		model.addAttribute("edicion", edicion);
+		model.addAttribute("alumno", alumnoEncontrado);
+		model.addAttribute("titulo","Modificar Alumno");
+		return "alumno";
+	}
+	
+	@PostMapping("/modificar")
+	public String modificarAlumno(@ModelAttribute("alumno") Alumno alumno) {
+		CollectionAlumno.modificarAlumno(alumno);
+		return "redirect:/alumno/listado";
+	}
+	
+	@GetMapping("/eliminar/{LU}")
+	public String eliminarCarrera(@PathVariable(value="LU") int lu) {
+		CollectionAlumno.eliminarAlumno(lu);
+		return "redirect:/alumno/listado";
+	}
+	
 }
