afcc94a61dd2d33e8ebafbe821e70354d1b4f2c3
EnzoVilla <enzovilla1810@gmail.com>
2024-06-21 23:35:06 -0300

    Error en el guardado de la clase mapper, correcion

--- tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/mapper/MateriaMapper.java
+++ tp4_BazanFabricioAgustin/src/main/java/ar/edu/unju/fi/mapper/MateriaMapper.java
@@ -1,5 +1,33 @@
 package ar.edu.unju.fi.mapper;
 
-public interface MateriaMapper {
+import java.util.List;
 
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.Mappings;
+
+import ar.edu.unju.fi.dto.MateriaDTO;
+import ar.edu.unju.fi.model.Materia;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface MateriaMapper {
+	@Mappings({
+		
+		@Mapping(source="cant_horas", target="cant_horas"),
+		@Mapping(source="carrera", target="carrera"),
+		@Mapping(source="cod", target="cod"),
+		@Mapping(source="docente", target="docente"),
+		@Mapping(source="modalidad", target="modalidad"),
+		@Mapping(source="nombre_materia", target="nombre_materia")
+	})
+	
+	MateriaDTO toMateriaDTO(Materia materia);
+	@InheritInverseConfiguration
+	Materia toMateria(MateriaDTO materiaDTO);
+	
+	List<MateriaDTO> toMateriaDTOList(List<Materia> materias);
+	List<Materia> toMateriaList(List<MateriaDTO> materiasDTO);
+	
 }
